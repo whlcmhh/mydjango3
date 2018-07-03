6b5d781db651c43f5bb923415d9bac7914c3611d
whlcmhh <whlcmhh@gmail.com>
2018-07-03 17:57:16 +0800

    third

---
+++ schedule/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+# Create your models here.
+
+class sch(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=30)
+    date=models.DateField()
+
+    class Meta:
+        ordring=('id',)
+
+    def __str__(self):
+        return self.name
+
+def tes():
+    pass
