a3093f88485a2eb767233ef0394eb54301fd9476
whlcmhh <whlcmhh@gmail.com>
2018-07-06 15:39:32 +0800

    add a commit

--- schedule/views.py
+++ schedule/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from django.core import serializers
 from schedule.models  import sch
+from schedule.serializers import schSerializer
 
 # Create your views here.
 
@@ -16,5 +17,7 @@ def add(request):
 
 def sche(request):
     sch_list=sch.objects.all()
-    sch_list_json=serializers.serialize('json',sch_list)
-    return HttpResponse(sch_list_json)
+    # sch_list_json=serializers.serialize('json',sch_list)
+    # return HttpResponse(sch_list_json)
+    sch_list_json=schSerializer(sch_list,many=True)
+    return JsonResponse(sch_list_json.data,safe=False)
